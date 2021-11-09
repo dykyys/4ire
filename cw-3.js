5c25d0b8467c9f033c36c1e563a06889509bf255
dykyys <dykyy.s@gmail.com>
2021-11-09 18:25:21 +0200

    add null, undefined

--- cw-3.js
+++ cw-3.js
@@ -1,24 +1,30 @@
-const array = ["hello", "world", 23, "23", null];
+const array = ["hello", "world", 23, "23", null, undefined];
 
 function filterBy(array, type) {
   const allTypes = [
     "string",
     "boolean",
     "number",
-    "undefined",
+    undefined,
     "object",
-    "null",
     "bigint",
+    null,
   ];
+
   if (!allTypes.includes(type)) {
     return `${type} такого типа данных не существует!`;
   }
   if (!Array.isArray(array)) {
     return `${array} не массив, мы не можем работать дальше!`;
   }
+  if (type === null || type === undefined) {
+    return array.filter((element) => element !== type);
+  }
   return array.filter((element) => typeof element !== type);
 }
-
-console.log(filterBy(array, "string"));
+console.log(array);
+console.log("filterBy string", filterBy(array, "string"));
 console.log(filterBy(array, "str"));
+console.log("filterBy null", filterBy(array, null));
 console.log(filterBy("str", "string"));
+console.log("filterBy undefined", filterBy(array, undefined));
